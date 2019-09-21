c6202838c77a7709cfcd307f053506cdfe72df8c
WhatProblem <18355350915@163.com>
2019-09-21 22:07:47 +0800

    泛型类泛型接口完善

--- TypeScript/src/Five.ts
+++ TypeScript/src/Five.ts
@@ -23,7 +23,7 @@ myClass.add(1)
 // myClass.add('2234') // 指定number后报错
 console.log(myClass.list)
 
-// 3.泛型接口：调用时通知接口定义的类型
+// 3.泛型接口：调用时通知接口定义的类型,定义方式1
 interface MyInterface<T> {
     (x: T, y: T): T
 }
@@ -33,6 +33,80 @@ let myFn: MyInterface<number> = function (x, y) {
 
 let three = myFn(1, 2)
 
+// 定义方式2：泛型函数接口
+interface ConfigFn {
+    <T>(value: T): T
+}
+
+let tFn: ConfigFn = function <T>(value: T) {
+    return value
+}
+tFn<string>('测试')
+
 export default {
     one, three
 }
+
+
+// 泛型类，类作为接口进行约束
+class ClassInterface {
+    title: string | undefined;
+    name: string | undefined;
+    status?: string | undefined;
+    constructor(param: {
+        title: string | undefined;
+        name: string | undefined;
+        status?: string | undefined;
+    }) {
+        this.title = param.title
+        this.name = param.name
+        this.status = param.status
+    }
+}
+
+class UseClassInterface<T>{
+    add(value: T): boolean {
+        return true
+    }
+}
+
+let cInterface = new ClassInterface({ title: '测试泛型类', name: '123' })
+cInterface.status = '200'
+let use = new UseClassInterface<ClassInterface>()
+use.add(cInterface)
+
+
+
+
+// 4.泛型接口，泛型类综合使用
+
+interface TUser<T> {
+    add(value: T): any[];
+    update(id: number): void;
+}
+
+class TClass<T> implements TUser<T>{
+    add(value: T): Array<any> {
+        return []
+    }
+    update(id: number) {
+
+    }
+}
+
+class Param {
+    id: number | undefined;
+    name: string | undefined;
+    constructor(param: {
+        id: number | undefined;
+        name: string | undefined;
+    }) {
+        this.id = param.id
+        this.name = param.name
+    }
+}
+
+let p = new Param({id: 123, name: '测试'})
+
+let tc = new TClass<Param>()
+tc.add(p)
