d333f32b2b4a4b0793e2c539e81b470cb294dff7
whatproblem <18355350915@163.com>
2020-06-21 12:28:26 +0800

    自动丢福

--- ts-base/src/basicDatatype/index.ts
+++ ts-base/src/basicDatatype/index.ts
@@ -20,11 +20,11 @@ const lists: Array<number> = [1, 2]
 
 // 5. 元组
 // Declare a tuple type
-// let x: [string, number];
+let x: [string, number]
 // Initialize it
-const x: [string, number] = ['hello', 10] // OK
+x = ['hello', 10] // OK
 // Initialize it incorrectly
-// x = [10, 'hello']; // Error
+x = [10, 'hello'] // Error
 
 // 6. 枚举
 // enum Color { Red, Green, Blue }
