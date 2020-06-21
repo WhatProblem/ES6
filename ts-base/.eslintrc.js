1799b870e306e9eaba3af232224f858e3d49f761
whatproblem <18355350915@163.com>
2020-06-21 12:07:36 +0800

    自动修复

--- ts-base/.eslintrc.js
+++ ts-base/.eslintrc.js
@@ -6,9 +6,8 @@ module.exports = {
     es6: true
   },
   extends: ["plugin:@typescript-eslint/recommended", "prettier/@typescript-eslint", "plugin:prettier/recommended"],
-  // extends: ["prettier", "plugin:prettier/recommended"],
   rules: {
-  //   "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    // "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     // "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-dupe-keys": "error",
     "no-duplicate-case": "error",
@@ -19,59 +18,6 @@ module.exports = {
     "curly": "error"
   },
   parserOptions: {
-    parser: "@typescript-eslint/parser",
-    // ecmaVersion: 2018,
-    // sourceType: 'module'
-  },
-  // plugins: [
-  //   "typescript"
-  // ]
+    parser: "@typescript-eslint/parser"
+  }
 };
-
-
-
-// module.exports = {
-//   root: true,
-//   parser: '@typescript-eslint/parser',
-//   plugins: [
-//     '@typescript-eslint',
-//   ],
-//   extends: [
-//     'eslint:recommended',
-//     'plugin:@typescript-eslint/recommended',
-//   ],
-// };
-
-
-// module.exports = {
-//   root: true,
-//   // parser: '@typescript-eslint/parser',
-//   env: {
-//     browser: true,
-//     es6: true,
-//     node: true
-//   },
-//   parserOptions: {
-//     ecmaVersion: 2018,
-//     sourceType: 'module'
-//   },
-//   plugins: [
-//     '@typescript-eslint',
-//   ],
-//   extends: [
-//     'eslint:recommended',
-//     'plugin:@typescript-eslint/recommended',
-//     'prettier/@typescript-eslint',
-//   ],
-//   rules: {
-//     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-//     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-//     "no-dupe-keys": "error",
-//     "no-duplicate-case": "error",
-//     "no-empty": ["error", { "allowEmptyCatch": true }],
-//     "no-ex-assign": "error",
-//     "no-extra-boolean-cast": "error",
-//     "no-extra-semi": "error",
-//     "curly": "error"
-//   },
-// };
